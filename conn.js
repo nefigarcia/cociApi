4326e5366416dc6c918c2abb62ea922ed55c0fdd
Nefi Garcia <bluedivee@hotmail.com>
2020-10-29 17:44:08 -0600

    gregado

--- conn.js
+++ conn.js
@@ -1,21 +1,47 @@
-const Pool=require('ps').Pool
+
+const Pool=require('pg').Pool
 const pool=new Pool({
     user: 'postgres',
-    host:'cocihdz.cwlc0akvrbty.us-east-2.rds.amazonaws.com' ,
-    database:'cocihnz' ,
-    password: 'avaricia971',
+    host:'cocihdz.cwlc0akvrbty.us-east-2.rds.amazonaws.com',
+    //'localhost' ,
+    database:'cocihnz',//'muebles' ,
+    password: 'avaricia971',//'dimi',
     port: '5432',
 });
 const getCon=()=>{
     return new Promise(function(resolve,reject){
-        pool.query('select * from usuarios',(error,results)=>{
+        pool.query('select id,nombre,descripcion,precio,fot,fotos from cocinas',(error,results)=>{
             if(error){
                 reject(error)
+                console.log("can't connect========")
             }
             resolve(results.rows);
         })
     })
 }
+const getUsu=()=>{
+    return new Promise(function(resolve,reject) {
+        pool.query('select * from usuarios ',(error,results)=>{
+            if(error){
+                reject(error)
+                console.log("no se encontro usuario");
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const postProd=(request,response)=>{
+    const{nombre,descripcion,precio,foto}=request.body;
+    const fotim=request.body.fotim; console.log(fotim);
+    pool.query('insert into cocinas(nombre,descripcion,precio,fot,fotos) values($1,$2,$3,$4,$5)',[nombre,descripcion,precio,foto,fotim],(error,results)=>{
+        if(error){
+            throw error;
+        }
+        response.status(201).send(`producto agregado`)
+    })
+}
+
 module.exports={
-    getCon
+    getCon,getUsu,postProd
 }
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express=require('express')
 const app=express()
 const port=3001
+var bodyParser=require('body-parser');
 
 const conn=require('./conn.js')
+
 app.use(express.json())
+app.use(bodyParser.json({limit:'50mb', extended:true}));
+app.use(bodyParser.urlencoded({limit:'50mb', extended: true }));
+
 app.use(function(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000'),
+    res.setHeader('Access-Control-Allow-Origin','http://www.cocinashdz.toltechconsulting.com'),//'http://localhost:3000'),
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE','OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
     next();
@@ -19,6 +24,16 @@ app.get('/',(req,res)=>{
         res.status(500).send(error);
     })
 })
+app.get('/usuario',(req,res)=>{
+    conn.getUsu()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+app.post('/regProd',conn.postProd)
 app.listen(port,()=>{
     console.log(`corriendo puerto ${port}`)
 })
